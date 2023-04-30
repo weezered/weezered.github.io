a6eff1de8960d41f583b313deac5767f11b05dc0
c0d44 <57160371+c0d44@users.noreply.github.com>
2023-04-30 20:41:52 +1000

    Update uisp.js

--- scripts/uisp.js
+++ scripts/uisp.js
@@ -1,7 +1,7 @@
 function ballsack() {
-  const sound = new Audio('/silly.mp3");
+  const sound = new Audio('/silly.mp3');
   sound.loop = true;
   sound.play();
 }
-
+ballsack();
 window.addEventListener('load', ballsack);
